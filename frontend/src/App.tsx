00fe33d2b2692ced4e2f2cacc9a742556fbd933b
Davi da Guarda <davi.guarda.b@hotmail.com>
2022-11-15 02:10:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/notificationButton"
 
 function App() {
-  return(
-    <h1>Olá, mundo!</h1>
+  return (
+    <>
+      <h1>Olá, mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
